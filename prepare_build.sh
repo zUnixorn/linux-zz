c4327e166852866aa139e026256be29b8ef1e6f7
Jonas Crull <dev@jonascrull.de>
2025-06-30 22:04:48 +0200

    include license and make META check more robust

--- prepare_build.sh
+++ prepare_build.sh
@@ -8,18 +8,18 @@ cd build
 gpg --import keys/pgp/*.asc
 
 # Check if the kernel version is supported
+zfs_tag=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/openzfs/zfs/releases/latest')
+zfs_tag=${zfs_tag##*/}
+
+curl -JLO "https://raw.githubusercontent.com/openzfs/zfs/refs/tags/${zfs_tag}/META"
 kernel_ver=$(sed -n 's/pkgver=\([0-9][0-9]*\.[0-9][0-9]*\).*/\1/p' PKGBUILD)
-curl -JLO 'https://raw.githubusercontent.com/openzfs/zfs/refs/heads/master/META'
 kernel_max=$(awk -F': *' '$1 == "Linux-Maximum" { print $2 }' META)
-zfs_newest=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/openzfs/zfs/releases/latest')
-zfs_newest=${zfs_newest##*/}
-zfs_newest=${zfs_newest#zfs-}
 
-echo "Using zfs version: zfs-${zfs_newest}"
+echo "Using zfs version: ${zfs_tag}"
 
-# TODO this check may not be robust enough, for example since the META file is obtained from master, the kernel_max may be different to the kernel_max in the release
+# Currently this check assumes that the versions do not need to be checked more than up to the minor version
 if [ "$kernel_ver" != "$(echo -e "${kernel_max}\n${kernel_ver}" | sort -V | head -n1)" ]; then
-	echo "Error: the kernel version is ${kernel_ver}, but zfs-${zfs_newest} only supports kernels up to version ${kernel_max}."
+	echo "Error: the kernel version is ${kernel_ver}, but ${zfs_tag} only supports kernels up to version ${kernel_max}."
 	exit 1
 fi
 
@@ -33,7 +33,7 @@ pkgbuild_checksum=$(sed -z \
 echo "Comparing with PKGBUILD hash: ${pkgbuild_checksum}"
 
 if [ "${PKGBUILD_SHA256SUM}" != "${pkgbuild_checksum}" ]; then
-	echo "Error: hash did not match"
+	echo "Error: hash did not match. Manual intervention is required"
 	exit 1
 fi
 
@@ -41,9 +41,10 @@ fi
 sed -i \
     -e "s/pkgbase=linux-zen/pkgbase=linux-zz/" \
     -e "s/pkgdesc='Linux ZEN'/pkgdesc='Linux ZEN with ZFS'/" \
-    -e "s/license=(GPL-2.0-only)/license=(GPL-2.0-only LicenseRef-CDDL)/" \
-    -e "1s/^/_zfsver=${zfs_newest}\n/" \
-    -e '$a\\nsource+=("https://github.com/openzfs/zfs/releases/download/zfs-${_zfsver}/zfs-${_zfsver}.tar.gz")\nsha256sums+=("SKIP")\nb2sums+=("SKIP")\n' \
-    -e 's/\(make -s kernelrelease > version\)/echo "Adding ZFS to tree..."; make prepare; cd ${srcdir}\/zfs-${_zfsver}; .\/autogen.sh; .\/configure CC=gcc --prefix=\/usr --sysconfdir=\/etc --sbindir=\/usr\/bin --libdir=\/usr\/lib --datadir=\/usr\/share --includedir=\/usr\/include --with-udevdir=\/lib\/udev --libexecdir=\/usr\/lib\/zfs --with-config=kernel --enable-linux-builtin=yes --with-linux=${srcdir}\/${_srcname} --with-linux-obj=${srcdir}\/${_srcname}; .\/copy-builtin ${srcdir}\/${_srcname}; cd ${srcdir}\/${_srcname}; .\/scripts\/config -e ZFS\n\n  \1/' \
+    -e "s/license=(GPL-2.0-only)/license=(GPL-2.0-only custom:CDDL)/" \
+    -e "1s/^/_zfsver=${zfs_tag}\n/" \
+    -e '$a\\nsource+=("https://github.com/openzfs/zfs/releases/download/${_zfsver}/${_zfsver}.tar.gz")\nsha256sums+=("SKIP")\nb2sums+=("SKIP")\n' \
+    -e 's/\(_package[a-zA-Z0-9\-]*() *{\)/\1\n  install -Dm644 ${srcdir}\/${_zfsver}\/LICENSE "${pkgdir}\/usr\/share\/licenses\/${pkgname}\/CDDL"/' \
+    -e 's/\(make -s kernelrelease > version\)/echo "Adding ZFS to tree..."; make prepare; cd ${srcdir}\/${_zfsver}; .\/autogen.sh; .\/configure CC=gcc --prefix=\/usr --sysconfdir=\/etc --sbindir=\/usr\/bin --libdir=\/usr\/lib --datadir=\/usr\/share --includedir=\/usr\/include --with-udevdir=\/lib\/udev --libexecdir=\/usr\/lib\/zfs --with-config=kernel --enable-linux-builtin=yes --with-linux=${srcdir}\/${_srcname} --with-linux-obj=${srcdir}\/${_srcname}; .\/copy-builtin ${srcdir}\/${_srcname}; cd ${srcdir}\/${_srcname}; .\/scripts\/config -e ZFS\n\n  \1/' \
     PKGBUILD
 
--- prepare_build_test.sh
+++ prepare_build_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This may be removed at a later date
+# Currently it is used for testing parts of the pipeline, since building the entire kernel for minor changes is infeasable
+
 mkdir build
 cd build
 curl -JLO 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=paru-bin'
